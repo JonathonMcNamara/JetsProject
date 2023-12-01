e73572099c1ca536c37f02540c299a3653c76dbe
JonathonMcNamara <jonathonmcnamara1234@hotmail.com>
2023-12-01 15:54:42 -0700

    Starting Files For JetsProject

---
+++ src/com/skilldistillery/jets/JetsApplication.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.jets;
+
+public class JetsApplication {
+	
+	private Airfield airfield;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
